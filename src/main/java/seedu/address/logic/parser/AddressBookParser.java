0d7b419ffc9214ac51e2432b816dd2eb1b3aa961
HiokKuek <128700697+HiokKuek@users.noreply.github.com>
2025-10-08 13:42:10 +0800

    Add priority command shortcut
    Implement 'priority INDEX PRIORITY' command as an alias for 'edit INDEX pr/PRIORITY' with full parsing and validation support.

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -79,7 +79,7 @@ public class AddressBookParser {
             return new HelpCommand();
         
         case PriorityCommand.COMMAND_WORD:
-            return new PriorityCommand();
+            return new PriorityCommandParser().parse(arguments);
 
         default:
             logger.finer("This user input caused a ParseException: " + userInput);
