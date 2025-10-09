81b99d0d702d99109f90626a632905c62b3b4f23
ChengLeong <e1398428@u.nus.edu>
2025-10-09 19:43:50 +0800

    checkstyle fixes

--- src/test/java/seedu/address/logic/parser/TagCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/TagCommandParserTest.java
@@ -5,10 +5,10 @@ import static seedu.address.logic.commands.CommandTestUtil.TAG_DESC_FRIEND;
 import static seedu.address.logic.commands.CommandTestUtil.TAG_DESC_HUSBAND;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_FRIEND;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
-import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
-import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
 
 import org.junit.jupiter.api.Test;
 
@@ -30,16 +30,16 @@ public class TagCommandParserTest {
 
     @Test
     public void parse_missingParts_failure() {
-        assertParseFailure(parser, " t/friend", MESSAGE_INVALID_FORMAT); // no index
-        assertParseFailure(parser, "1", MESSAGE_INVALID_FORMAT);         // no tag
-        assertParseFailure(parser, "", MESSAGE_INVALID_FORMAT);          // empty input
+        assertParseFailure(parser, " t/friend", MESSAGE_INVALID_FORMAT);
+        assertParseFailure(parser, "1", MESSAGE_INVALID_FORMAT);
+        assertParseFailure(parser, "", MESSAGE_INVALID_FORMAT);
     }
 
     @Test
     public void parse_invalidPreamble_failure() {
-        assertParseFailure(parser, "-1 t/friend", MESSAGE_INVALID_FORMAT);  // negative index
-        assertParseFailure(parser, "0 t/friend", MESSAGE_INVALID_FORMAT);   // zero index
-        assertParseFailure(parser, "abc t/friend", MESSAGE_INVALID_FORMAT); // non-numeric
+        assertParseFailure(parser, "-1 t/friend", MESSAGE_INVALID_FORMAT);
+        assertParseFailure(parser, "0 t/friend", MESSAGE_INVALID_FORMAT);
+        assertParseFailure(parser, "abc t/friend", MESSAGE_INVALID_FORMAT);
     }
 
     @Test
@@ -50,14 +50,14 @@ public class TagCommandParserTest {
     @Test
     public void parse_invalidTagValue_failure() {
         assertParseFailure(parser, "1 t/!!invalid!!",
-        String.format(MESSAGE_INVALID_COMMAND_FORMAT, TagCommand.MESSAGE_USAGE));
+            String.format(MESSAGE_INVALID_COMMAND_FORMAT, TagCommand.MESSAGE_USAGE));
     }
     // ---------- SUCCESS CASES ----------
 
     @Test
     public void parse_validSingleTag_success() {
         Index targetIndex = INDEX_FIRST_PERSON;
-        String userInput = targetIndex.getOneBased() + TAG_DESC_FRIEND; // "1 t/friend"
+        String userInput = targetIndex.getOneBased() + TAG_DESC_FRIEND;
         TagCommand expectedCommand = new TagCommand(targetIndex, new Tag(VALID_TAG_FRIEND));
         assertParseSuccess(parser, userInput, expectedCommand);
     }
