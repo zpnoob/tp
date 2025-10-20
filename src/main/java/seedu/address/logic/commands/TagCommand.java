ee87ad9c0d8242e0c3f7f1b046de89cfa64f947f
lanjia0 <nicholas.chin.wai.kit@gmail.com>
2025-10-20 18:20:04 +0800

    Debug Tag and Priority command
    to integrate new age field

--- src/main/java/seedu/address/logic/commands/TagCommand.java
+++ src/main/java/seedu/address/logic/commands/TagCommand.java
@@ -64,7 +64,8 @@ public class TagCommand extends Command {
                 personToEdit.getEmail(),
                 personToEdit.getAddress(),
                 newTagsSet,
-                personToEdit.getPriority()
+                personToEdit.getPriority(),
+                personToEdit.getAge()
         );
 
         model.setPerson(personToEdit, editedPerson);
