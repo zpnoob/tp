fe6666789a197167034ee60e2c3d7356f353b04c
lanjia0 <nicholas.chin.wai.kit@gmail.com>
2025-10-20 20:35:35 +0800

    Update GUI to reflect age field

--- src/test/java/seedu/address/logic/commands/TagCommandTest.java
+++ src/test/java/seedu/address/logic/commands/TagCommandTest.java
@@ -29,7 +29,7 @@ public class TagCommandTest {
     private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
 
     @Test
-    public void execute_validInfexUnfilteredList_success() {
+    public void execute_validIndexUnfilteredList_success() {
         Person personToEdit = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
         Tag newTag = new Tag("friend");
         TagCommand tagCommand = new TagCommand(INDEX_FIRST_PERSON, newTag);
@@ -49,6 +49,7 @@ public class TagCommandTest {
                     .collect(Collectors.joining(", "));
         String expectedMessage = String.format(TagCommand.MESSAGE_TAG_PERSON_SUCCESS,
                                         editedPerson.getName() + "; Phone: " + editedPerson.getPhone()
+                                            + "; Age: " + editedPerson.getAge()
                                             + "; Priority: " + editedPerson.getPriority()
                                            + "; Email: " + editedPerson.getEmail()
                                            + "; Address: " + editedPerson.getAddress()
