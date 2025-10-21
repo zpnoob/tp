ced18e5d22866c816c57c4141e55fec0d5ed46a4
zamien <pehzamie@gmail.com>
2025-10-21 18:07:33 +0800

    Modify Parser and Cli to include lcd

--- src/main/java/seedu/address/logic/parser/ParserUtil.java
+++ src/main/java/seedu/address/logic/parser/ParserUtil.java
@@ -15,6 +15,7 @@ import seedu.address.model.person.Name;
 import seedu.address.model.person.Phone;
 import seedu.address.model.person.Priority;
 import seedu.address.model.tag.Tag;
+import seedu.address.model.person.LastContactedDate;
 
 /**
  * Contains utility methods used for parsing strings in the various *Parser classes.
@@ -133,5 +134,20 @@ public class ParserUtil {
             tagSet.add(parseTag(tagName));
         }
         return tagSet;
+    }
+
+    /**
+     * Parses a {@code String date} into a {@code LastContactedDate}.
+     * Leading and trailing whitespaces will be trimmed.
+     *
+     * @throws ParseException if the given {@code date} is invalid.
+     */
+    public static LastContactedDate parseLastContactedDate(String date) throws ParseException {
+        requireNonNull(date);
+        String trimmedDate = date.trim();
+        if (!LastContactedDate.isValidLastContactedDate(trimmedDate)) {
+            throw new ParseException(LastContactedDate.MESSAGE_CONSTRAINTS);
+        }
+        return new LastContactedDate(trimmedDate);
     }
 }
