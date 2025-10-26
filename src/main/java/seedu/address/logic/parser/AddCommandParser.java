ba0d3f812047e2cc383343dc5c55b1ddf2359cef
ChengLeong <e1398428@u.nus.edu>
2025-10-26 23:46:02 +0800

    Add IncomeBracket to BetterModelClassDiagram

--- src/main/java/seedu/address/logic/parser/AddCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddCommandParser.java
@@ -1,4 +1,4 @@
-package seedu.address.logic.parser;
+﻿package seedu.address.logic.parser;
 
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
